daef6a83911174dc20b3d09a403112bcc2c89221
BogdanYarotsky <byarotsky@gmail.com>
2024-04-29 23:52:08 +0200

    deserializing telegram update and storing only text in cosmos db

--- Translate.cs
+++ Translate.cs
@@ -1,5 +1,7 @@
 using System.Globalization;
 using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
@@ -39,28 +41,87 @@ namespace Trans
             var resp = req.CreateResponse(HttpStatusCode.OK);
             resp.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            //try
-            //{
-            //    var translation = await _client.GermanToEnglishAsync("Apfel", default);
-            //    await resp.WriteStringAsync($"Welcome to {translation} Functions!");
-            //}
-            //catch (Exception e)
-            //{
-            //    await resp.WriteStringAsync(e.ToString());
-            //}
-
             using var reader = new StreamReader(req.Body);
             var body = await reader.ReadToEndAsync();
+            var update = JsonSerializer.Deserialize<TelegramUpdate>(body);
+
             return new MultiResponse
             {
                 HttpResponse = resp,
                 Document = new MyDocument
                 {
-                    partitionKey = DateTime.Now.ToString(CultureInfo.InvariantCulture),
-                    id = "Gnida",
-                    lobzik = body
+                    partitionKey = Guid.NewGuid().ToString(),
+                    lobzik = update?.Message.Text ?? string.Empty,
                 }
             };
         }
     }
+
+    public class Chat
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("type")]
+        public string Type { get; set; }
+    }
+
+    public class From
+    {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("is_bot")]
+        public bool IsBot { get; set; }
+
+        [JsonPropertyName("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonPropertyName("last_name")]
+        public string LastName { get; set; }
+
+        [JsonPropertyName("username")]
+        public string Username { get; set; }
+
+        [JsonPropertyName("language_code")]
+        public string LanguageCode { get; set; }
+    }
+
+    public class Message
+    {
+        [JsonPropertyName("message_id")]
+        public int MessageId { get; set; }
+
+        [JsonPropertyName("from")]
+        public From From { get; set; }
+
+        [JsonPropertyName("chat")]
+        public Chat Chat { get; set; }
+
+        [JsonPropertyName("date")]
+        public int Date { get; set; }
+
+        [JsonPropertyName("text")]
+        public string Text { get; set; }
+    }
+
+    public class TelegramUpdate
+    {
+        [JsonPropertyName("update_id")]
+        public int UpdateId { get; set; }
+
+        [JsonPropertyName("message")]
+        public Message Message { get; set; }
+    }
+
+
 }
